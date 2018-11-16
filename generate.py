d63edfe9a7739b9ba7d792133563654a1bb428f8
Ilana Bromberg <ilana.bromberg@stonybrook.edu>
2018-11-15 21:26:29 -0500

    Add files via upload

--- generate.py
+++ generate.py
@@ -6,37 +6,92 @@ import random
 ingred_input = open("ingredients.txt","r")
 all_ingredients = ingred_input.readlines()
 
+titleB = ['Infatuation','Sleep','Fertility','Health','Wisdom','Peace','Healing','Transformation','Luck','Hair Growth','Safety']
+
 i = 0 # ye olde index variable
 j=0
+ingredients_list = []
 
-title_printout = ' '
+
+def generate_positive():
+    positive_ttl = ['Poultice','Balm','Charm','Glamour','Enchantment']
+    p2 = ['Love','Rest','Health','Wisdom','Peace','Healing','Luck','Hair Growth','Safety']
+    
+    titlechoice = random.randint(0,1)
+    if titlechoice == 0:
+        return random.choice(positive_ttl) + ' of ' + random.choice(p2)
+    else:
+        return random.choice(positive_ttl) + ' for ' + random.choice(p2)
+    
+def generate_neutral():
+    neutral_ttl = ['Spell','Elixer','Potion']
+    p2 = ['Love','Rest','Health','Wisdom','Peace','Healing','Luck','Hair Growth','Safety','Infatuation','Death','Sickness','Acne']
+    
+    titlechoice = random.randint(0,1)
+    if titlechoice == 0:
+        return random.choice(neutral_ttl) + ' of ' + random.choice(p2)
+    else:
+        return random.choice(neutral_ttl) + ' for ' + random.choice(p2)
+    
+def generate_negative():
+    negative_ttl = ['Hex','Curse','Blight','Plague']
+    p2 = ['Infatuation','Death','Sickness','Acne']
+    
+    titlechoice = random.randint(0,1)
+    if titlechoice == 0:
+        return random.choice(negative_ttl) + ' of ' + random.choice(p2)
+    else:
+        return random.choice(negative_ttl) + ' for ' + random.choice(p2)
 
 def generate():
     ingredient_printout = ''
+    """ --------------- intent generation --------- """
     
-    """ --------------- TITLE CHOICES --------- """
-    titleA = ['Spell','Potion','Elixer','Poultice','Hex','Curse']
-    titleB = ['Love','Sleep','Fertility','Health','Wisdom']
-    titlechoice = random.randint(0,1)
-    if titlechoice == 0:
-        title_printout = random.choice(titleA) + ' of ' + random.choice(titleB) + '\n'
+    # determine if positive, neutral, or negative spell
+    intent = random.randint(0,2)
+    if intent == 0: 
+        titleA = generate_positive()
+    elif intent == 1:
+        titleA = generate_neutral()
     else:
-        title_printout = random.choice(titleA) + ' for ' + random.choice(titleB) + '\n'
-        
-        
+        titleA = generate_negative()
+            
     """ --------------- INGREDIENT CHOICES --------- """
-    # possible_ingredients = ingred_input.read().splitlines()
-        
-    num_Ingred = random.randint(3,8) # randomly generates how many ingredients to choose
-    ingredients = random.sample(range(0,len(all_ingredients)),num_Ingred) #array of random ingredients without repeating
     
-    for i in range (0,num_Ingred):
+    
+    num_components = random.randint(4,7) # randomly generates how many ingredients to choose    
+    ingredients = random.sample(range(0,len(all_ingredients)),num_components) # picks out num_comp amount of integers within 0 to the length of the ingredients list
+    
+    for i in range (0,num_components):
         ingredient_printout += all_ingredients[ingredients[i]]
-    return title_printout + '\n' + ingredient_printout
+        
+        ingredients_list.append(all_ingredients[ingredients[i]])
+        
+    """ GENERATE INSTRUCTIONS """
+    
+    potential = ['grind together','mix together','stir together']
+    potential2 = ['under the light of a full moon', 'under the light of a new moon','in a cauldron']
+    potential3 = ['with a steady hand','while praying to a deity of your choice']
+    
+    num_components=4
+    which_instructions = random.randint(0,1)
+    if num_components == 4:
+        instructions = ("First, take the " + ingredients_list[0] + " and the " + ingredients_list[1]
+        + " and " + random.choice(potential) + ". Next, add the " + ingredients_list[2] + ' ' + random.choice(potential2)
+        + " and add " + ingredients_list[3] )
+    elif num_components == 5:
+        instructions = "lol"
+    elif num_components == 6:
+        instructions = "what"
+    else:
+        instructions = "weener"
+    
+    return titleA + '\n\n' + ingredient_printout + '\n\n' + instructions
     
 
 """ --------------- OUTPUT --------- """
 print("A LITTLE ROBOT'S GUIDE TO WITCHCRAFT \n             ------------ \n")
-for j in range (0,10): print(generate() + "\n             ------------ \n") # print ingredients list
+for j in range (0,2): 
+    print(generate() + "\n\t\t------------\n") # print ingredients list
 
 ingred_input.close()
